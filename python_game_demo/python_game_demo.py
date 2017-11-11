c8deb303ba25d15eaec7b093beef72f4b9b5ba04
tangshihui <761034176@qq.com>
2017-11-11 11:09:50 +0800

    python game demo

---
+++ python_game_demo/python_game_demo.py
@@ -0,0 +1,28 @@
+﻿import sys
+import pygame
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    pygame.init()
+
+    ai_setting=Settings()
+    screen=pygame.display.set_mode((ai_setting.screen_width,ai_setting.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    ship=Ship(ai_setting,screen)
+    bullets=Group()
+
+    while True:
+        gf.check_events(ai_setting,screen,ship,bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        print(len(bullets))
+
+        gf.update_screen(ai_setting,screen,ship,bullets)
+
+
+run_game()
